16d69a1a5a55e559c4851176a96e60070c194c4e
FABIÁN MATÍAS VEGA ALCOTA <fabian.vegaa@sansano.usm.cl>
2021-07-06 22:19:19 -0400

    Added get_importation function

--- src/generator.py
+++ src/generator.py
@@ -1,2 +1,19 @@
-def generate_imports_diagrams():
-    pass
+import yaml
+from yaml.loader import SafeLoader
+
+import importlib
+
+
+def get_importation(section: str, subsection: str):
+    with open("classes_diagrams.yaml", "r") as file:
+        imports = yaml.load(file, Loader=SafeLoader)
+
+    importation = imports.get(section, dict()).get(subsection, None)
+
+    mod = importlib.import_module(importation.split(" ")[1])
+    return eval("mod.{}".format(importation.split(" ")[-1]))
+
+
+mod = get_importation("aws", "adconnector")
+
+print(mod.__name__, dir(mod))
